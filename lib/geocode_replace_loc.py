1cdfb0f57b9210e7f07dd4cb3bcd6f809da20365
Kevin Johnson <vadskye@gmail.com>
2013-07-11 16:03:56 -0700

    Minor reorganization of geocode_replace_loc

--- lib/geocode_replace_loc.py
+++ lib/geocode_replace_loc.py
@@ -7,21 +7,34 @@
 import datetime
 import sys
 
+# TODO: Find a way to unit test this set of queries
+def create_loc_and_locmerge_tables(cursor):
+
+    create_table_temp2(cursor)
+    update_table_locmerge(cursor)
+    create_table_temp3(cursor)
+
+    # cleanup, we'll leave that in here.
+    cursor.executescript("""
+        DROP TABLE IF EXISTS loc;
+
+        CREATE TABLE loc AS SELECT * FROM temp3;
+
+        CREATE INDEX IF NOT EXISTS loc_idxCC ON loc (City, Country);
+        CREATE INDEX IF NOT EXISTS loc_idx   ON loc (City, State, Country);
+        CREATE INDEX IF NOT EXISTS loc_idxCS ON loc (City, State);
+
+        DROP TABLE IF EXISTS temp2;
+        DROP TABLE IF EXISTS temp3;
+          """)
+
 # TODO: Unit test this so that it and the unit test can be
 # eliminated in a future redesign. Also, ensure that this
 # is the correct name for this function, and adjust accordingly.
-
-def print_table_info(c):
-    field = ["[%s]" % x[1] for x in c.execute("PRAGMA TABLE_INFO(temp1)")][2:6]
-    var_f = ",".join(field)
-    print "var_f: ", var_f
-
-
-def print_loc_and_merge(cursor):
-    VarX = cursor.execute("select count(*) from loc").fetchone()[0]
-    VarY = cursor.execute("select count(*) from locMerge").fetchone()[0]
-    print " - Loc =", VarX, " OkM =", VarY, " Total =", VarX+VarY, "  ", datetime.datetime.now()
-
+#def print_table_info(c):
+#    field = ["[%s]" % x[1] for x in c.execute("PRAGMA TABLE_INFO(temp1)")][2:6]
+#    var_f = ",".join(field)
+#    print "var_f: ", var_f
 
 def create_table_temp2(cursor):
     cursor.executescript("""
@@ -37,7 +50,6 @@ def create_table_temp2(cursor):
 
         CREATE INDEX IF NOT EXISTS t2_idx ON temp2 (CityA, StateA, CountryA);
           """)
-
 
 def update_table_locmerge(cursor):
     cursor.executescript("""
@@ -66,7 +78,6 @@ def update_table_locmerge(cursor):
                AND  a.CountryA = b.CountryA;
           """)
 
-
 def create_table_temp3(cursor):
     cursor.executescript("""
         CREATE TEMPORARY TABLE temp3 AS
@@ -77,28 +88,12 @@ def create_table_temp3(cursor):
                AND  a.State   = b.State
                AND  a.Country = b.Country;
           """)
+    
+def print_loc_and_merge(cursor):
+    VarX = cursor.execute("select count(*) from loc").fetchone()[0]
+    VarY = cursor.execute("select count(*) from locMerge").fetchone()[0]
+    print " - Loc =", VarX, " OkM =", VarY, " Total =", VarX+VarY, "  ", datetime.datetime.now()
 
-
-# TODO: Find a way to unit test this set of queries
-def create_loc_and_locmerge_tables(cursor):
-
-    create_table_temp2(cursor)
-    update_table_locmerge(cursor)
-    create_table_temp3(cursor)
-
-    # cleanup, we'll leave that in here.
-    cursor.executescript("""
-        DROP TABLE IF EXISTS loc;
-
-        CREATE TABLE loc AS SELECT * FROM temp3;
-
-        CREATE INDEX IF NOT EXISTS loc_idxCC ON loc (City, Country);
-        CREATE INDEX IF NOT EXISTS loc_idx   ON loc (City, State, Country);
-        CREATE INDEX IF NOT EXISTS loc_idxCS ON loc (City, State);
-
-        DROP TABLE IF EXISTS temp2;
-        DROP TABLE IF EXISTS temp3;
-          """)
 
 def domestic_sql():
 
