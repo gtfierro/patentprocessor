9ee1167b37abe0e991b6acaa815197b632b99baf
= <billwyeh@yahoo.com>
2013-09-04 16:12:21 -0700

    add fields, helper functions to application handler

--- lib/handlers/application_handler_v1.py
+++ lib/handlers/application_handler_v1.py
@@ -36,7 +36,7 @@ class PatentGrant(object):
         self.country = self.xml.publication_reference.contents_of('country', upper=False)[0]
         self.patent = xml_util.normalize_document_identifier(self.xml.publication_reference.contents_of('doc_number')[0])
         self.kind = self.xml.publication_reference.contents_of('kind')[0]
-        self.date_applied = self.xml.publication_reference.contents_of('date')[0] # changed from grant to applied
+        self.date_app = self.xml.publication_reference.contents_of('date')[0] # changed from grant to applied
         self.pat_type = None # not sure exactly what this is
         # don't know what to do about referencing a patent grant
         # or when the patent has been granted
@@ -54,29 +54,102 @@ class PatentGrant(object):
             "type": self.pat_type,
             "number": self.patent,
             "country": self.country,
-            "date": self._fix_date(self.date_grant),
+            "date": self._fix_date(self.date_app),
             "abstract": self.abstract,
             "title": self.invention_title,
             "kind": self.kind,
             "num_claims": self.clm_num
         }
+        self.app = { # again, don't know what to do about referencing patent grant
+            "type": 'FakeType',
+            "number": str(3453),
+            "country": 'FakeCountry',
+            "date": self._fix_date(self.date_app)
+        }
+        self.app["id"] = str(self.app["date"])[:4] + "/" + self.app["number"]
 
-        def _fix_date(self, datestring):
-            """
-            Converts a number representing YY/MM to a Date
-            """
-            if not datestring:
-                return None
-            elif datestring[:4] < "1900":
-                return None
-            # default to first of month in absence of day
-            if datestring[-4:-2] == '00':
-                datestring = datestring[:-4] + '01' + datestring[-2:]
-            if datestring[-2:] == '00':
-                datestring = datestring[:6] + '01'
-            try:
-                datestring = datetime.strptime(datestring, '%Y%m%d')
-                return datestring
-            except Exception as inst:
-                print inst, datestring
-                return None
+    def _invention_title(self):
+        original = self.xml.contents_of('invention_title', upper=False)[0]
+        if isinstance(original, list):
+            original = ''.join(original)
+        return original
+
+    def _name_helper(self, tag_root):
+        """
+        Returns dictionary of firstname, lastname with prefix associated
+        with lastname
+        """
+        firstname = tag_root.contents_of('first_name', as_string=True, upper=False)
+        lastname = tag_root.contents_of('last_name', as_string=True, upper=False)
+        return xml_util.associate_prefix(firstname, lastname)
+
+    def _name_helper_dict(self, tag_root):
+        """
+        Returns dictionary of firstname, lastname with prefix associated
+        with lastname
+        """
+        firstname = tag_root.contents_of('first_name', as_string=True, upper=False)
+        lastname = tag_root.contents_of('last_name', as_string=True, upper=False)
+        firstname, lastname = xml_util.associate_prefix(firstname, lastname)
+        return {'name_first': firstname, 'name_last': lastname}
+
+    def _fix_date(self, datestring):
+        """
+        Converts a number representing YY/MM to a Date
+        """
+        if not datestring:
+            return None
+        elif datestring[:4] < "1900":
+            return None
+        # default to first of month in absence of day
+        if datestring[-4:-2] == '00':
+            datestring = datestring[:-4] + '01' + datestring[-2:]
+        if datestring[-2:] == '00':
+            datestring = datestring[:6] + '01'
+        try:
+            datestring = datetime.strptime(datestring, '%Y%m%d')
+            return datestring
+        except Exception as inst:
+            print inst, datestring
+            return None
+
+    @property
+    def assignee_list(self):
+        """
+        Returns list of dictionaries:
+        assignee:
+          name_last
+          name_first
+          residence
+          nationality
+          organization
+          sequence
+        location:
+          id
+          city
+          state
+          country
+        """
+        assignees = self.xml.assignees.assignee
+        if not assignees:
+            return []
+        res = []
+        for i, assignee in enumerate(assignees):
+            # add assignee data
+            asg = {}
+            asg.update(self._name_helper_dict(assignee))  # add firstname, lastname
+            asg['organization'] = assignee.contents_of('orgname', as_string=True, upper=False)
+            asg['role'] = assignee.contents_of('role', as_string=True)
+            asg['nationality'] = assignee.nationality.contents_of('country')[0]
+            asg['residence'] = assignee.nationality.contents_of('country')[0]
+            # add location data for assignee
+            loc = {}
+            for tag in ['city', 'state', 'country']:
+                loc[tag] = assignee.contents_of(tag, as_string=True, upper=False)
+            #this is created because of MySQL foreign key case sensitivities
+            loc['id'] = unidecode(u"|".join([loc['city'], loc['state'], loc['country']]).lower())
+            if any(asg.values()) or any(loc.values()):
+                asg['sequence'] = i
+                asg['uuid'] = str(uuid.uuid1())
+                res.append([asg, loc])
+        return res
