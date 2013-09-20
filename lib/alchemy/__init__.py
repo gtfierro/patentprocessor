0878c86becacdbdbfa7358c07e675efa08793b59
= <billwyeh@yahoo.com>
2013-09-20 16:06:39 -0700

    add code for us classifications

--- lib/alchemy/__init__.py
+++ lib/alchemy/__init__.py
@@ -253,10 +253,10 @@ def add_application(obj, override=True, temp=False):
 def add_all_app_fields(obj, app):
     add_app_asg(obj, app)
     add_app_inv(obj, app)
-    """
     # add_app_law(obj, app)
     add_app_usreldoc(obj, app)
     add_app_classes(obj, app)
+    """
     add_app_ipcr(obj, app)
     # add_app_citations(obj, app)
     add_app_claims(obj, app)
