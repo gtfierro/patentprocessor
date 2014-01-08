e1cb72abebece765354db078b13d6d0efc947b4a
Gabe Fierro <gtfierro225@gmail.com>
2014-01-08 14:52:06 -0800

    integrate celery/redis into run_clean.sh

--- run_clean.sh
+++ run_clean.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-echo 'Running assignee disambiguation'
-for i in {a..z} ; do
-    python lib/assignee_disambiguation.py $1 $i
-done
+cd lib
+celery -A tasks worker --loglevel=info --logfile=celery.log --concurrency=3 &
+celeryPID=$!
+redis-server &
+redisPID=$!
+cd ..
+echo $celeryPID > celery.pid
+echo $redisPID > redis.pid
+
+python lib/assignee_disambiguation.py $1
+#echo 'Running assignee disambiguation'
+#for i in {a..z} ; do
+#    python lib/assignee_disambiguation.py $1 $i
+#    sleep 2
+#done
+
+kill $celeryPID  # kill celery
+kill $redisPID # kill redis
+rm lib/dump.rdb # remove redis dump
+# remove pid files
+rm celery.pid redis.pid
 
 if [ $1 = "grant" ]
     then
