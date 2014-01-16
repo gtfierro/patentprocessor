1c7b9c82606c298d4a945e217ba0dc77849970f2
Gabe Fierro <gtfierro225@gmail.com>
2014-01-16 11:11:14 -0800

    use default lawyer disambig

--- run_clean.sh
+++ run_clean.sh
@@ -16,15 +16,13 @@ cd ..
 echo $celeryPID > celery.pid
 echo $redisPID > redis.pid
 
+echo 'Running assignee disambiguation'
 python lib/assignee_disambiguation.py $1
 
-#if [ $1 == "grant" ]
-#    then
-#        echo 'Running lawyer disambiguation'
-#        python lib/lawyer_disambiguation.py
-#        #for i in {a..z} ; do
-#        #done
-#fi
+if [ $1 == "grant" ] ; then
+  echo 'Running lawyer disambiguation'
+  python lib/lawyer_disambiguation.py $1
+fi
 
 echo 'Running geo disambiguation'
 python lib/geoalchemy.py $1
