9aa2fe102159068d4a1d94c237bc6bbf8e68f39d
Gabe Fierro <gtfierro225@gmail.com>
2014-02-08 14:06:30 -0800

    do not need redis/celery

--- run_clean.sh
+++ run_clean.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-dbtype=`awk -F " = " '/database/ {print $2}' lib/alchemy/config.ini | head -n 1`
-
-cd lib
-if [ $dbtype == "sqlite" ] ; then
-  celery -A tasks worker --loglevel=info --logfile=celery.log --concurrency=1 &
-  celeryPID=$!
-else
-  celery -A tasks worker --loglevel=info --logfile=celery.log --concurrency=3 &
-  celeryPID=$!
-fi
-redis-server &
-redisPID=$!
-cd ..
-echo $celeryPID > celery.pid
-echo $redisPID > redis.pid
-
 echo 'Running assignee disambiguation'
 python lib/assignee_disambiguation.py $1
 
@@ -26,9 +10,3 @@ fi
 
 echo 'Running geo disambiguation'
 python lib/geoalchemy.py $1
-
-kill $celeryPID  # kill celery
-kill $redisPID # kill redis
-rm lib/dump.rdb # remove redis dump
-# remove pid files
-rm celery.pid redis.pid
