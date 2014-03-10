ea99bdc94af28f9f8261c819eb131b465cda686f
Gabe Fierro <gtfierro225@gmail.com>
2014-03-10 10:34:15 -0700

    add disambiguated data to invpat file

--- get_invpat.py
+++ get_invpat.py
@@ -14,13 +14,14 @@ session = session_generator()
 #                left join uspc on uspc.patent_id = patent.id \
 #                left join inventor on inventor.id = rawinventor.inventor_id \
 #                where uspc.sequence = 0;')
-res = session.execute('select rawinventor.name_first, rawinventor.name_last, rawlocation.city, rawlocation.state, \
-                rawlocation.country, rawinventor.sequence, patent.id, year(application.date), \
+res = session.execute('select rawinventor.name_first, rawinventor.name_last, location.city, location.state, \
+                location.country, rawinventor.sequence, patent.id, year(application.date), \
                 year(patent.date), rawassignee.organization, uspc.mainclass_id, inventor.id \
-                from rawinventor, rawlocation, patent, application, rawassignee, uspc, inventor \
+                from rawinventor, rawlocation, patent, application, rawassignee, uspc, inventor,location \
                 where rawinventor.patent_id = patent.id and \
                 application.patent_id = patent.id and \
                 rawlocation.id = rawinventor.rawlocation_id and \
+                location.id = rawlocation.location_id and \
                 rawassignee.patent_id = patent.id and \
                 uspc.patent_id = patent.id and \
                 inventor.id = rawinventor.inventor_id;')
